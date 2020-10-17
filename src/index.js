4315de03ad21bed2243dd27231995da403da1f2a
vladislav-volodchenko <70842775+vladislav-volodchenko@users.noreply.github.com>
2020-10-17 11:44:19 +0300

    init: start brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ 	let stack = [],
+    	brackets = Object.fromEntries(bracketsConfig);
+    for (let val of str.split('')) {
+        if (stack[0] === val) {
+            stack.shift();
+        } else if (brackets[val]) {
+            stack.unshift(brackets[val]);
+        } else {
+            return false;
+        }
+    }
+    return stack.length === 0;
 }
+
+//https://js-algorithms.tutorialhorizon.com/2015/11/16/justify-if-a-string-consists-of-valid-parentheses/
